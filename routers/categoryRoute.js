8ab525708a612fbbd21e2c6e80865b70b2e28b5b
Ekdvs <ekdvsampath02@gmail.com>
2025-08-26 19:08:53 +0530

    Refactor category creation: separate image handling, add get all categories and update category functionality

--- routers/categoryRoute.js
+++ routers/categoryRoute.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { createCategory } from '../controllers/categoryController';
+import admin from '../middleweare/admin';
+import auth from '../middleweare/auth';
+import upload from '../middleweare/multer';
 const categoryRouter = express.Router(); 
 
 //create category
-categoryRouter.post('/create',createCategory);
+categoryRouter.post('/create',auth,admin ,upload.single("image"), createCategory);
 
+//get all category
 
 export default categoryRouter;
