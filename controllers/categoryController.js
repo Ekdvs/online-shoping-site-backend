3ecddd89177a42b0e56b2545948765c86576d39e
Ekdvs <ekdvsampath02@gmail.com>
2025-08-26 19:08:53 +0530

    Refactor category creation: separate image handling, add get all categories and update category functionality

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,10 +4,11 @@ import Category from "../models/category.model";
 //create category
 export const createCategory=async (request,response)=>{
     try {
-        const{name,image}=request.body;
+        const{name}=request.body;
+        const imageFile =request.file;
 
         //check name and image exist
-        if(!name||!image){
+        if(!name||!imageFile){
             return response.status(400).json({
                 message:'Name and Image provide',
                 erroe:true,
@@ -26,10 +27,12 @@ export const createCategory=async (request,response)=>{
             })
         }
 
+        //upload image cloudinary 
+        const uploadedImage = await uploadImageCloudinary(imageFile);
         //create payload
         const payload={
             name,
-            image
+            image:uploadedImage.secure_url,
 
         }
         //create catalogy in database
@@ -52,3 +55,82 @@ export const createCategory=async (request,response)=>{
         });
     }
 }
+
+//get all category
+export const getAllCategories=async(request,response)=>{
+    try {
+        const categories=await Category.find();
+
+        return response.status(200).json({
+            message:'retview all categories',
+            data:categories,
+            error:false,
+            success:true,
+        })
+        
+    } catch (error) {
+        return response.status(500).json({
+            message:'Server error'+error.message,
+            error:true,
+            success:false,
+        })
+        
+    }
+
+}
+
+//update category
+export const updateCategory=async(request,response)=>{
+    try {
+        //get details from
+        const name= request.body;
+
+        //find category from dat base
+        const category =await Category.findOne({name});
+
+        if(!category){
+            return response.status(404).json({
+                message:'Category not found',
+                error:true,
+                success:false,
+            })
+        }
+
+        // image url
+        let imageurl=category.image;
+
+        //if add new image
+        if(request.file){
+            if(imageurl){
+                const publicId = category.image.split("/").pop().split(".")[0];
+                await cloudinary.uploader.destroy(`online-shopping-site/${publicId}`);
+            }
+            // Upload new image
+            const uploaded = await uploadImageCloudinary(req.file);
+            imageurl = uploaded.secure_url;
+
+        }
+
+        category.name = name || category.name;
+        category.image = imageurl;
+        const updated = await category.save();
+
+        return response.status(200).json({
+            message:'Category updated',
+            data:updated,
+            error:false,
+            success:true,
+        })
+
+
+    } catch (error) {
+        return response.status(500).json({
+            message:"server error"+error.message,
+            error:true,
+        })
+        
+    }
+}
+
+// delete category
+export 
