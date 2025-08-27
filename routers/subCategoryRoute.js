6584e1cdfea53ad03dfe34902a737d7e421ba545
Ekdvs <ekdvsampath02@gmail.com>
2025-08-27 17:06:00 +0530

    Implement subcategory management: add delete, get all, and search by name functionalities

--- routers/subCategoryRoute.js
+++ routers/subCategoryRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 import auth from '../middleweare/auth.js';
 import admin from '../middleweare/admin.js';
 import upload from '../middleweare/multer.js';
-import { createSubCategory, updateSubCategory } from '../controllers/SubCategoryController.js';
+import { createSubCategory, deleteSubCategory, getAllSubCategories, getSubCategoryByName, updateSubCategory } from '../controllers/SubCategoryController.js';
 const subCategoryRouter = express.Router(); 
 
 // Create subcategory (Admin only, with image upload)
@@ -11,5 +11,13 @@ subCategoryRouter.post("/create", auth, admin, upload.single("image"), createSub
 // Update subcategory (Admin only, with optional image upload)
 subCategoryRouter.put("/update/:id", auth, admin, upload.single("image"), updateSubCategory);
 
+//searchsubcstegory by name
+subCategoryRouter.get('/search/:name',getSubCategoryByName);
+
+//get all subcategories
+subCategoryRouter.get('/getall',getAllSubCategories);
+
+//delete subcategory(admin only)
+subCategoryRouter.delete('/delete/:id',auth,admin,deleteSubCategory)
 
 export default subCategoryRouter;
