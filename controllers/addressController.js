757a2d687add9b5956f7e10b83e58aba080cb709
Ekdvs <ekdvsampath02@gmail.com>
2025-08-28 20:39:44 +0530

    Add address retrieval, update, and deletion functionality with user ID validation

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -70,3 +70,128 @@ export const getAddresses=async(request,response)=>{
 }
 
 // Get Single Address by ID
+export const getAddressById=async(request,response)=>{
+    try {
+       const userId=request.userId;
+
+        //check user id
+        if (!userId) {
+            return res.status(400).json({
+                message: "User ID is required",
+                error: true,
+                success: false,
+            });
+        }
+
+        //get address
+        const {addressId}=request.params;
+
+        const address=await AddressModel.findById(addressId);
+
+        if(!address){
+            return res.status(400).json({
+                message: "Address not found",
+                error: true,
+                success: false,
+            });
+        }
+
+        return response.status(200).json({
+            message: "Address retrieved successfully",
+            data: address,
+            success: true,
+            error:false,
+        })
+
+
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong",
+            error: true,
+            success: false,
+        }); 
+    }
+}
+
+//update adress
+export const updateAddress=async(request,response)=>{
+    try {
+
+        const userId=request.userId;
+        const {addressId}=request.params;
+
+        //check user id
+        if (!userId) {
+            return res.status(400).json({
+                message: "User ID is required",
+                error: true,
+                success: false,
+            });
+        }
+
+        const updated=await AddressModel.findByIdAndUpdate(addressId,request.body);
+
+        if(!updated){
+             return res.status(400).json({
+                message: "adress update fail",
+                error: true,
+                success: false,
+            });
+        }
+
+        return response.status(200).json({
+                    message: "Address updated successfully",
+                    data: updated,
+                    success: true,
+                    });
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong",
+            error: true,
+            success: false,
+        }); 
+    }
+
+
+}
+
+//delete adress
+export const deleteAddress=async(request,response)=>{
+    try {
+        const userId=request.userId;
+        const {addressId}=request.params;
+
+        //check user id
+        if (!userId) {
+            return res.status(400).json({
+                message: "User ID is required",
+                error: true,
+                success: false,
+            });
+        }
+
+        //delete from data base
+        const deleted=await AddressModel.findByIdAndDelete(addressId)
+        if(!deleted){
+            return res.status(400).json({
+                message: "Address not found",
+                error: true,
+                success: false,
+            });
+        }
+
+        return response.status(200).json({
+                    message: "Address deleted successfully",
+                    success: true,
+                    });
+    } 
+    catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong",
+            error: true,
+            success: false,
+        }); 
+    }
+}
