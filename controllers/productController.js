56fa0b22ddeeb2856248940eabe9c65135ae1947
Ekdvs <ekdvsampath02@gmail.com>
2025-08-28 11:51:08 +0530

    Implement product management features: add get product by ID, update product, delete product, search products by name or description, and filter products by category, price, and stock.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-import { response } from "express";
+import { request, response } from "express";
 import Product from "../models/product.model.js";
 import uploadImageCloudinary from "../util/uploadImageCloudinary.js";
 
@@ -89,3 +89,214 @@ export const getAllProducts=async(request,response)=>{
             });
     }
 }
+
+//getproductbyid
+export const getProductById=async(request,responce)=>{
+    try {
+        //get product id
+        const {productId}=request.params
+
+        const product=await Product.findById(productId)
+        .populate("categoryId", "name")
+         .populate("sub_categoryId", "name");
+
+        //check product avalibility
+        if(!product){
+            return response.status(404).json({
+                message:'product not found',
+                error:true,
+                success:false,
+            })
+        }
+
+        return responce.status(200).json({
+            message:'retview the product',
+            data: product, 
+            error: false, 
+            success: true 
+        })
+
+
+        
+    } 
+    catch (error) {
+        return response.status(500).json({ 
+             message: "Server error",
+             error: error.message 
+            });
+    }
+}
+
+//update product
+export const updateProduct=async(request,response)=>{
+    try {
+        const{id}=request.params;
+        let updateData={...request.body}
+
+        //check id
+        if(!id){
+            return res.status(404).json({ 
+                message: "product id not found" ,
+                error:true,
+                success:false,
+            })
+        }
+
+        // If new images are uploaded
+    if (request.files && request.files.length > 0) {
+      const uploadedImages = await Promise.all(
+        req.files.map((file) => uploadImageCloudinary(file))
+      );
+      updateData.image = uploadedImages.map((img) => img.secure_url);
+    }
+
+    const updated = await Product.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updated) {
+      return res.status(404).json({ 
+        message: "Product not found" ,
+        error:true,
+        success:false,
+    });
+    }
+
+    return response.status(200).json({
+        message: "Product updated successfully",
+      data: updated,
+      error: false,
+      success: true,
+    })
+
+    } 
+    catch (error) {
+        return response.status(500).json({ 
+             message: "Server error",
+             error: error.message 
+            });
+    }
+}
+
+//delete product
+export const deleteproduct=async(request,response)=>{
+    try {
+            const{id}=request.params;
+       
+
+        //check id
+        if(!id){
+            return res.status(404).json({ 
+                message: "product id not found" ,
+                error:true,
+                success:false,
+            })
+        }
+
+        //delete from data base
+
+        const productdeleted=await Product.findByIdAndDelete(id);
+
+        if(!productdeleted){
+             return res.status(404).json({ 
+                message: " can not find product" ,
+                error:true,
+                success:false,
+            })
+        }
+
+        return response.status(200).json({
+            message: "Product deleted successfully",
+            error:false,
+            success:true,
+        })
+
+
+        
+    } catch (error) {
+         return response.status(500).json({ 
+             message: "Server error",
+             error: error.message 
+            });
+    }
+}
+
+//search product by by name or description
+export const searchProduct=async(request,response)=>{
+    try {
+        //get keyword
+        const {keyword}=request.body;
+        if(!keyword){
+            return response.status(404).json({
+                message:'Can not find key word',
+                error:true,
+                success:false
+            })
+        }
+        const products=await Product.find({
+            $or:[
+                {name:{$regex:keyword,$option:'i'}},
+                {description:{$regex:keyword,$option:'i'}}
+            ],
+        })
+
+        //avaliable check
+        if(!products){
+            return response.status(404).json({
+                message:'Can not find products this keyword',
+                error:true,
+                success:false
+            })
+        }
+
+        return response.status(200).json({
+            message:'Search results',
+            data:products,
+            error:false,
+            success:true,
+        })
+        
+    } 
+    catch (error) {
+         return response.status(500).json({ 
+             message: "Server error",
+             error: error.message 
+            });
+    }
+}
+
+//filter products by category ,price,stock,etc
+export const fillterProducts=async(request,response){
+    try {
+
+        // fiter by results
+        const { category, minPrice, maxPrice, inStock } = req.query;
+        let filters={};
+
+        if (category) filters.categoryId = category;
+        if (inStock) filters.stock = { $gt: 0 };
+        if (minPrice || maxPrice) {
+            filters.price = {};
+            if (minPrice) filters.price.$gte = Number(minPrice);
+            if (maxPrice) filters.price.$lte = Number(maxPrice);
+        }
+
+        const products =await product.find(filters);
+
+        return response.status(200).json({
+            message: "Filtered products",
+            data: products,
+            error: false,
+            success: true,
+        })
+        
+    } 
+    catch (error) {
+         return response.status(500).json({ 
+             message: "Server error",
+             error: error.message 
+            });
+    }
+}
+
