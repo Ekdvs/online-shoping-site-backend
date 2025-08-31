69fb878abf8000256a89bec880ee6cd5c166d099
Ekdvs <ekdvsampath02@gmail.com>
2025-08-31 21:34:19 +0530

    update

--- configs/db.js
+++ configs/db.js
@@ -1,26 +1,18 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+const connectDB=async()=>{
+    if(!process.env.MONGO_URL){
+        throw new Error('please defind MONGO_URl variabel inside the .env file')
+    }
+    
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('sucessfull connect to database');
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+    }
+    catch(error){
+        console.error("Database Connection failed" ,error);
+        
+    }
 }
-
-const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URL not defined");
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URL).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
-
 export default connectDB;
