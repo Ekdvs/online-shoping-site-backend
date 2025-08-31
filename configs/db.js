ecfb3190ff81e4b9947c0dd563511bb5059cf1a7
Ekdvs <ekdvsampath02@gmail.com>
2025-08-31 17:14:10 +0530

    update

--- configs/db.js
+++ configs/db.js
@@ -6,7 +6,7 @@ if (!cached) cached = global.mongoose = { conn: null, promise: null };
 const connectDB = async () => {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URL).then(m => m);
+    cached.promise = mongoose.connect('mongodb+srv://stu0752678:rg2a8kWUOtzG3BCd@cluster0.kughe.mongodb.net/onlineShop?retryWrites=true&w=majority&appName=cluster0').then(m => m);
   }
   cached.conn = await cached.promise;
   return cached.conn;
