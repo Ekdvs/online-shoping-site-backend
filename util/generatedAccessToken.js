03a559ab93544e46bc0f5c44dafef9f3cd805c51
Ekdvs <ekdvsampath02@gmail.com>
2025-09-01 15:06:18 +0530

    update  the login from

--- util/generatedAccessToken.js
+++ util/generatedAccessToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generatedAccesToken =async(userId)=>{
     const token=await jwt.sign({id:userId}
         ,process.env.SECRET_KEY_ACCESS_TOKEN,
-        {expiresIn:'1h'}
+        {expiresIn:'15m'}
     )
     return token;
 }
