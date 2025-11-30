cd05663d3d7c463a7734106b18cfd8af8c2487ee
Ekdvs <ekdvsampath02@gmail.com>
2025-11-30 09:38:43 +0530

    Fix syntax error in mailer.js

--- mailer.js
+++ mailer.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     }
-    console.log(host,port,secure,auth)
+    
 
 });
 
@@ -26,4 +26,5 @@ transporter.verify((error, success) => {
 
 
 export default transporter;
+
 
